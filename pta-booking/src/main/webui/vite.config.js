4cd723b3a49019253b84bb4b1c6bfcec7a65b3ab
Olivier von Dach <olivier.vondach@obya.ch>
2024-05-03 21:41:14 +0200

    added license, build pipeline by microservice

--- pta-booking/src/main/webui/vite.config.js
+++ pta-booking/src/main/webui/vite.config.js
@@ -1,3 +1,25 @@
+/*-
+ * #%L
+ * pta-booking
+ * $Id:$
+ * $HeadURL:$
+ * %%
+ * Copyright (C) 2024 obya
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
 // This config is here because there is already an index in META-INF/resources/index.html
 // If you want this as the index, remove META-INF/resources/
 // Then you can remove this config file and rename quinoa.html to index.html
